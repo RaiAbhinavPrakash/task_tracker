0b7a8f3c34733bc6247a6f69efa67098f00cca28
Abhinav Prakash Rai <79394301+RaiAbhinavPrakash@users.noreply.github.com>
2022-05-31 20:01:31 +0530

    Update Tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,27 +1,6 @@
 import Task from "./Task"
 
 const Tasks = ({ tasks, onDelete, onToggle  }) => {
-    // const[tasks, setTasks] = useState([
-            // {
-            //     id: 1,
-            //     text: "Doctor's Appointment",
-            //     day: 'Feb 5th at 2:30pm',
-            //     reminder: true,
-            // },
-            // {
-            //     id: 2,
-            //     text: "Meeting at school",
-            //     day: 'Feb 6th at 1:30pm',
-            //     reminder: true,
-            // },
-            // {
-            //     id: 3,
-            //     text: "trading",
-            //     day: 'Feb 7th at 10:30am',
-            //     reminder: false,
-            // }
-   //   ]
-    // )
   return (
     <>
         {tasks.map((task) => (
